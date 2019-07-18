25c4f67405c12ec7ae9b47ab2036f6df34fbee6b
1234qwer <paul.pan@9client.com>
2019-07-18 12:19:24 +0800

    demo code

---
+++ Spring-Boot-Elasticsearch-Demo-1.0/src/main/java/cn/deep/demo/dao/BookRepository.java
@@ -0,0 +1,20 @@
+package cn.deep.demo.dao;
+
+import cn.deep.demo.entity.BookBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * 接口关系：
+ * ElasticsearchRepository --> ElasticsearchCrudRepository --> PagingAndSortingRepository --> CrudRepository
+ */
+public interface BookRepository extends ElasticsearchRepository<BookBean, String> {
+
+    //Optional<BookBean> findById(String id);
+
+    Page<BookBean> findByAuthor(String author, Pageable pageable);
+
+    Page<BookBean> findByTitle(String title, Pageable pageable);
+
+}
---
+++ Spring-Boot-Elasticsearch-Demo-1.0/src/main/java/cn/deep/demo/service/BookService.java
@@ -0,0 +1,26 @@
+package cn.deep.demo.service;
+
+import cn.deep.demo.entity.BookBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookService {
+
+    Optional<BookBean> findById(String id);
+
+    BookBean save(BookBean blog);
+
+    void delete(BookBean blog);
+
+    Optional<BookBean> findOne(String id);
+
+    List<BookBean> findAll();
+
+    Page<BookBean> findByAuthor(String author, PageRequest pageRequest);
+
+    Page<BookBean> findByTitle(String title, PageRequest pageRequest);
+}
+
